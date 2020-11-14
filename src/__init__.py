6267c2c3e1306e7e2236680ad05c7a101eff4172
guillaumeBadikian <guillaume.badikian@gmail.com>
2020-11-14 21:21:20 +0100

    feat : parsing

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+
+import time
+from src.Parsing import Parsing
+
+
+if __name__ == '__main__':
+    #nltk.download('stopwords')
+    search = b"<doc>"
+    result = []
+    r = []
+
+    start = time.time()
+    with open("./data/Text_Only_Ascii_Coll_MWI_NoSem","r") as f:
+        p = Parsing()
+        p.parse(f)
+        w = "States"
+        print("time execution {}".format(time.time() - start))
+        print(p.getTf("10003934", w))
+        print(p.getIdf(w))
+        print(p.getWeight("10003934",w))
+        print("time execution {}".format(time.time()-start))
+    #[print(i) for i in r]
+
+
+
